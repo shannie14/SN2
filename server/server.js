7644f8b7df5066c459da8431969d8f8607cbe785
Shannon Kendall <shannon.marie.kendall@gmail.com>
2022-11-06 16:22:58 -0500

    initial clean up

--- server/server.js
+++ server/server.js
@@ -1,45 +1,37 @@
 const express = require('express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-// Import the ApolloServer class
-const { ApolloServer } = require('apollo-server-express');
 
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-});
-
-const connectionStringURI = process.env.NODE_ENV == 'production' ? process.env.MONGODB_URL : `mongodb://127.0.0.1:27017/NBTV`;
-
 const app = express();
 
+// app.use(routes);
 app.use(express.static(path.join(__dirname, './client')));
 app.use(express.json());
 
-// app.use(routes);
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'index.html')));
-}
-
 //mongoose connection
+const connectionStringURI = process.env.NODE_ENV == 'production' ? process.env.MONGODB_URL : `mongodb://127.0.0.1:27017/NBTV`;
+
 mongoose.connect(connectionStringURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('SK connected to mongodb successfully')
         console.log(Object.keys(mongoose.connection.collections));
     })
 
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
 
 // Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app });
